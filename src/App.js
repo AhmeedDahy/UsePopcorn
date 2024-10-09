8631c367ea8ec0e217a42fbc20464213d6d42594
Ahmed S.Dahy <115500490+AhmeedDahy@users.noreply.github.com>
2024-10-09 09:45:14 +0300

    fixing vercel problem with http request

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       async function getMovieData() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
